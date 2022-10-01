3b48c789773227a98c9abaf4814a3545ee5267c2
nitis <nitish8152@gmail.com>
2022-10-01 09:18:17 +0530

    Addrees book system

---
+++ src/com/bridgelab/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        //Welcome message for Users
+        System.out.println("---Welcome to Address Book System Problem---");
+    }
+}
