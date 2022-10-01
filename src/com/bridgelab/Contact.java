f0854ee00d2890f588dc42ad8e6e0a525c826a96
nitis <nitish8152@gmail.com>
2022-10-01 09:24:08 +0530

    Created Contact, AddressBook Class with Variables

---
+++ src/com/bridgelab/Contact.java
@@ -0,0 +1,16 @@
+package com.bridgelab;
+//Ability to create a Contacts in Address
+//Book with first and last names, address,
+//city, state, zip, phone number and email.
+
+public class Contact {
+    //Variable Declaration for Contacts Class
+    String firstName;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNo;
+    String emailID;
+}
